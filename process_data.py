ce37c794c7996f5b6e4dba5b76e8fc6450af3a65
Marian <marian.grobar@avesoft.cz>
2021-05-15 16:59:37 +0200

    ohraniceni na obrazku

--- process_data.py
+++ process_data.py
@@ -1,4 +1,6 @@
+import math
 from datetime import datetime
+from PIL import Image, ImageDraw
 
 import pandas
 import json
@@ -15,10 +17,16 @@ class Identification:
     def __str__(self):
         return f'X: {self.x} Y: {self.y} width: {self.width} height: {self.height} description: {self.description}'
 
+
+def GetDistance(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+
+
 data_dictionary = dict()
 
 # exported classification.csv
-data = pandas.read_csv('C:/Users/AnetaOpletalova/Desktop/VoynichExports/voynich-manuscript-classifications.csv')
+data = pandas.read_csv('D:\\Downloads\\voynich-manuscript-classifications (2).csv')
 for index, row in data.iterrows():
 
     # filter out dates before selected date
@@ -49,6 +57,63 @@ for index, row in data.iterrows():
 
 for key, values in data_dictionary.items():
     print(key)
-    for value in values:
-        print(value)
+    if key == "f082v.jpg":
+        groups = []
+        done = []
+        for c1 in range(len(values)):
+            print(values[c1])
+            c1x0 = values[c1].x
+            c1y0 = values[c1].y
+            c1x1 = c1x0 + values[c1].width
+            c1y1 = c1y0 + values[c1].height
+            avgx0 = 0
+            avgy0 = 0
+            avgx1 = 0
+            avgy1 = 0
+            group = []
+            avgCoordinates = []
+            for c2 in range(c1, len(values)):
+                if c1 != c2:
+                    c2x0 = values[c2].x
+                    c2y0 = values[c2].y
+                    c2x1 = c2x0 + values[c2].width
+                    c2y1 = c2y0 + values[c2].height
+
+                    euc1 = GetDistance(c1x0,c1y0,c2x0,c2y0)
+                    euc2 = GetDistance(c1x1,c1y1,c2x1,c2y1)
+
+                    if euc1 < 10 and euc2 < 10:
+                        avgx0 = avgx0 + c2x0
+                        avgy0 = avgy0 + c2y0
+                        avgx1 = avgx1 + c2x1
+                        avgy1 = avgy1 + c2y1
+
+                        group.append(c2)
+                        done.append(c2)
+
+            if c1 not in done:
+                avgx0 = avgx0 + c1x0
+                avgy0 = avgy0 + c1y0
+                avgx1 = avgx1 + c1x1
+                avgy1 = avgy1 + c1y1
+                group.append(c1)
+
+                avgCoordinates = [avgx0/len(group),avgy0/len(group),avgx1/len(group),avgy1/len(group)]
+
+
+                group.append(avgCoordinates)
+                done.append(c1)
+
+            if len(group) != 0:
+                groups.append(group)
+
+        with Image.open("f082v.jpg") as im:
+            draw = ImageDraw.Draw(im)
+            for gr in groups:
+                x0 = gr[-1][0]
+                y0 = gr[-1][1]
+                x1 = gr[-1][2]
+                y1 = gr[-1][3]
+                draw.rectangle([(x0, y0), (x1, y1)], width=2, outline='red')
+            im.show()
 
